8316c7ede84b1fcb8f6476e897e0d4829857fedf
kiderman <49405235+kiderman@users.noreply.github.com>
2019-04-18 13:40:56 +0300

    Update simonSense.py

--- simonSense.py
+++ simonSense.py
@@ -86,6 +86,7 @@ def vaildate_player_moves(leds, listOfcolors):
                 return False
             else:
                     i += 1
+                    sleep(0.5)
 
     #if he followed all the colors return true to continue the game
     sleep(0.5)
@@ -101,7 +102,7 @@ def check_sensors():
             #sensorValues[0] = fireV
             print("fire: ", fireV)
             print(dSensors["fire"])
-            sleep(0.1)
+            sleep(0.2)
             return dSensors["fire"]
         #check if the light sensor value changed
         lightV = mcp.read_adc(1)
@@ -109,7 +110,7 @@ def check_sensors():
            # sensorValues[1] = lightV
             print("light: ", lightV)
             print(dSensors["light"])
-            sleep(0.1)
+            sleep(0.2)
             return dSensors["light"]
         #check if the voice sensor value changed
         voiceV = mcp.read_adc(2)
@@ -117,7 +118,7 @@ def check_sensors():
            # sensorValues[2] = voiceV
             print("voice: ", voiceV)
             print(dSensors["voice"])
-            sleep(0.1)
+            sleep(0.2)
             return dSensors["voice"]
         #check if the potentiometer sensor value changed
         potentiometerV = mcp.read_adc(3)
@@ -125,7 +126,7 @@ def check_sensors():
           #  sensorValues[3] = potentiometerV
             print("pot: ", potentiometerV)
             print(dSensors["potentiometer"])
-            sleep(0.1)
+            sleep(0.2)
             return dSensors["potentiometer"]
         
 
